c93ba59404d90fc965c71e7b9fff748e55f7412a
Kevin Arnold Arias Figueroa <kaaf030191@gmail.com>
2024-11-05 15:44:42 -0500

    Feat: 05-11-2024 - Consumo de servicio de API.
    Consumo de servicio de API a través de un componente de Angular.

---
+++ src/app/api/person.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, retry } from 'rxjs';
+
+@Injectable({
+	providedIn: 'root'
+})
+
+export class PersonService {
+	constructor(
+		private httpClient: HttpClient
+	) {}
+
+	public getData(): Observable<any> {
+		return this.httpClient.get(`http://localhost:8080/person/getdata`).pipe(retry(3));
+	}
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PersonService } from './api/person.service';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: 'app-root',
+	standalone: true,
+	imports: [RouterOutlet],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
-  title = 'appcwds220242';
+	person: any = {};
+
+	constructor(
+		private personService: PersonService
+	) {}
+
+	ngOnInit() {
+		this.personService.getData().subscribe({
+			next: (response: any) => {
+				this.person = response;
+			},
+			error: (error: any) => {
+				console.log(error);
+			}
+		});
+	}
 }
